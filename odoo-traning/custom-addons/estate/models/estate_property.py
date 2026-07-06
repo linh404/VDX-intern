a6a79aa5616423e28ebf27915e677cf8e1b20b55
linhxeom <luuhoanglinh.2003@gmail.com>
2026-07-06 21:24:13 +0700

    create estate_property module

--- odoo-traning/custom-addons/estate/models/estate_property.py
+++
@@ -1,5 +0,0 @@
-from odoo import models
-
-
-class EstateProperty(models.Model):
-    _name = "estate.property"
---
+++ odoo-traning/custom-addons/my-estate/models/estate_property.py
@@ -0,0 +1,68 @@
+from dateutil.relativedelta import relativedelta
+
+from odoo import fields, models
+
+
+class EstateProperty(models.Model):
+    _name = 'estate.property'
+    _description = 'Test Model'
+
+    name = fields.Char(string="tên")
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(
+        copy=False,
+        default=lambda self: fields.Date.today() + relativedelta(month=3),
+    )
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(
+        required=True,
+        copy=False
+    )
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        [
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
+        ]
+    )
+    active = fields.Boolean(default=True)
+
+    state = fields.Selection(
+        [
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
+        ],
+        required=True,
+        copy=False,
+        default="new",
+    )
+
+    property_type_id = fields.Many2one("estate.property.type", string="Property Type")
+
+    buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
+
+    salesperson_id = fields.Many2one(
+        "res.users",
+        string="Salesperson",
+        default=lambda self: self.env.user,
+    )
+
+    tag_ids = fields.Many2many("estate.property.tag", string="Tags")
+
+    offer_ids = fields.One2many(
+        "estate.property.offer",
+        "property_id",
+        string="Offers",
+    )
+
