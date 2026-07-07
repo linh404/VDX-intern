dc678bc6896245048b376b2becfedb0d158eb7d9
linhxeom <luuhoanglinh.2003@gmail.com>
2026-07-07 14:46:59 +0700

    update

--- odoo-traning/custom-addons/my-estate/models/estate_property_tag.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class EstatePropertyTag(models.Model):
-    _name = "estate.property.tag"
-    _description = "Real Estate Property Tag"
-
-    name = fields.Char(required=True)
--- odoo-traning/custom-addons/my-estate/models/estate_property_type.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class EstatePropertyType(models.Model):
-    _name = "estate.property.type"
-    _description = "Real Estate Property Type"
-
-    name = fields.Char(required=True)
