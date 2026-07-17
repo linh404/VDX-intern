6baed3aeea4c9cfe181d23e8b3c33c9c27bcaed5
linhxeom <luuhoanglinh.2003@gmail.com>
2026-07-17 16:51:02 +0700

    update

--- odoo-traning/custom_addons/my_estate/models/__init__.py
+++ odoo-traning/custom_addons/my_estate/models/__init__.py
@@ -2,5 +2,3 @@ from . import estate_property
 from . import estate_property_tag
 from . import estate_property_offer
 from . import estate_property_type
-from . import estate_property_cancel_wizard
-from . import estate_property_report_wizard
---
+++ odoo-traning/custom_addons/my_estate/wizard/__init__.py
@@ -0,0 +1,2 @@
+from . import estate_property_cancel_wizard
+from . import estate_property_report_wizard
